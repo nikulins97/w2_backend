152537988a057b5d08ec367b12cb50c89b339587
sergei.nikulin <nsi.work46@gmail.com>
2026-04-24 01:09:35 +0300

    remove async from signToken and verifyToken

--- utils/token.js
+++ utils/token.js
@@ -1,10 +1,10 @@
 const jwt = require('jsonwebtoken');
 
-async function signToken(payload, secret, options) {
+function signToken(payload, secret, options) {
     return jwt.sign(payload, secret, options);
 }
 
-async function verifyToken(token, secret) {
+function verifyToken(token, secret) {
     return jwt.verify(token, secret);
 }
 
